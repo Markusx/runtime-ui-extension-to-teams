f9e00e19f0fd67ed0943e8ef01393a0bad1e3298
Markusx <markusbk@hotmail.com>
2017-09-19 15:52:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -82,7 +82,7 @@ var main = function (params) {
         break;
             
         case ACTION_SUBMIT:
-          //  return submit(params);
+            return submit(params);
         break;
   
         case ACTION_ENCRYPT:
