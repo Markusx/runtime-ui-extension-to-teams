71f7a4a79af156c986424f92357719b570bac1e4
Markusx <markusbk@hotmail.com>
2017-09-19 13:08:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,17 @@ var main = function (params) {
 
     var request = require("request-promise");
    
+    var request = require('request');
+    var url ='https://requestb.in/1gurela1'
+    var options = {
+        uri:url,
+        form:{body:params}
+    }
+    request(url, function (error, response, body) {
+      if (!error) {
+        console.log(body);
+      }
+    });
 
     if ( params.action == 'sendMessage' ) {
              
